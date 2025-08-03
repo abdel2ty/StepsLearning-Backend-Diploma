01c2867d052bae5c8c8212bb08ac18df1ede2aa4
abdelaty7 <muhvmmvdessvm2@gmail.com>
2025-08-03 18:12:17 +0300

    3-8-2025

---
+++ c-sharp/01/lecture1.cs
@@ -0,0 +1,25 @@
+// =============================================================
+// Default
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+// =============================================================
+// Print Statement
+static void Main(string[] args)
+{
+  System.Console.WriteLine("Enter Your Name:");
+  Console.ReadLine(); //Takes from user Input
+}
